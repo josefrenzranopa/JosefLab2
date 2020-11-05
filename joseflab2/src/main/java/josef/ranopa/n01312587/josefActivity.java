9f49eb2de8b7ba12a7274d1253d007c397aad849
josefrenzranopa <n01312587@humbermail.ca>
2020-11-05 18:02:03 -0500

    first commit

---
+++ joseflab2/src/main/java/josef/ranopa/n01312587/josefActivity.java
@@ -0,0 +1,16 @@
+// josef Renz Ranopa N01312587 CENG 319-RNB
+
+package josef.ranopa.n01312587;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class josefActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_josef);
+    }
+}
